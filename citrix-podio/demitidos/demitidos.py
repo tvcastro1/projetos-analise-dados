7467968bb3af37cd56fdcccdac5a7acf9bd10eac
tvcastro1 <thiagovieirac@gmail.com>
2022-09-20 22:43:33 -0300

    Remodelagem das classes para operar com um grau mais alto de abstração

--- citrix-podio/demitidos/demitidos.py
+++ citrix-podio/demitidos/demitidos.py
@@ -1,21 +1,15 @@
 import pandas as pd
 from pypodio2 import api
+from dataframe import retorna_dados_dos_demitidos
 
-CLIENT_ID = ''
-TOKEN = ''
-USER = ''
-PASSWORD = ''
+CLIENT_ID = 'tablets'
+TOKEN = 'jQdHRUi7z0HxW7EL01PDj8HMqnFuV3oB2L8WwjmryxKUkR1TsmNs5G6UiLufnR9n'
+USER = 'vieira.thiago@parceiro.mrv.com.br'
+PASSWORD = '1Y!rqint'
 
 dataframe_demitidos = pd.read_csv('demitidos.csv', dtype=str)
-dataframe_demitidos = dataframe_demitidos.drop_duplicates(subset=['Nome Funcionários'],keep='first')
-filtro_dataframe_demitidos = dataframe_demitidos.filter(items=['Nome Funcionários'], axis=1). \
-    apply(lambda x: x.astype(str)). \
-    drop_duplicates(keep='first', inplace=False, subset=None)
+dataframe_demitidos = dataframe_demitidos.drop_duplicates(subset=['Nome Funcionários'], keep='first')
 
-dataframe_atuais = pd.read_csv('atuais.csv', dtype=str)
-
-dataframe_chefia = dataframe_atuais[['NOMEFUNCIONARIO', 'EMAIL']].set_index('NOMEFUNCIONARIO').\
-    fillna('Sem e-mail cadastrado')
 
 
 class Demitidos(object):
@@ -28,27 +22,11 @@ class Demitidos(object):
 
     @classmethod
     def verifica_demitido(cls, funcionario):
-        row = dataframe_demitidos.loc[dataframe_demitidos['Nome Funcionários'] == funcionario]
-        dict_demitido = row[['Nome Funcionários', 'Nome Superior', 'Data Demissão']].to_dict('list')
-        nome, superior, dt_demissao = [val[0] for val in dict_demitido.values()]
-        chefe = row['Nome Superior']
-        email_chefe = dataframe_chefia.loc[chefe].to_dict('list')
-        email_chefe = [val[0] for val in email_chefe.values()][0]
+        nome, superior, dt_demissao, email_chefe = retorna_dados_dos_demitidos(funcionario)
         return cls(nome, superior, dt_demissao, email_chefe)
-
-    @property
-    def email_chefia(self):
-        return self._email_superior
-
-    @email_chefia.setter
-    def email_chefia(self, email):
-        self._email_superior = email
 
     def show_demitidos(self):
         return vars(self)
-    # def __str__(self):
-    #     return f'O funcionário {self.nome} foi demitido em {self.dt_demissao}. {self.superior}, favor realizar ' \
-    #            f'a transferência e seu email é {self._email_superior}'
 
 
 class Podio(object):
@@ -58,14 +36,9 @@ class Podio(object):
         self.nome = nome
         self.patrimonios = patrimonios
 
-    @staticmethod
-    def inicializa_client():
-        client = api.OAuthClient(CLIENT_ID, TOKEN, USER, PASSWORD)
-        return client
-
     @classmethod
     def retorna_dicionario_equipamento_colaborador(cls, colaborador):
-        filtro = cls.client.Item.filter(24231717, {'filters': {203968038: colaborador, 203968026:[1]}})
+        filtro = cls.client.Item.filter(24231717, {'filters': {203968038: colaborador, 203968026: [1]}})
         if filtro['filtered'] > 0:
             chave = []
             valor = []
@@ -80,7 +53,7 @@ class Podio(object):
             codigos_de_patrimonio_do_dataframe = df.loc[df.shift(1)['Valor'] == 'Patrimônio']['Valor'] \
                 .to_frame()
             dicionario_patrimonios_do_dataframe = codigos_de_patrimonio_do_dataframe.to_dict('records')
-            #### Bloco de extração dos dígitos de patrimônio do DataFrame ####
+            # Bloco de extração dos dígitos de patrimônio do DataFrame #
             c = 0
             novo_dicionario_de_patrimonios = {}
             lista_patrimonios = []
@@ -104,8 +77,11 @@ class Podio(object):
         except AttributeError:
             pass
 
+
 def iterar_dataframe():
+    output = pd.DataFrame()
     for _, row in dataframe_demitidos.iterrows():
+
         func_obj = Demitidos.verifica_demitido(row['Nome Funcionários'])
         dict_a = func_obj.show_demitidos()
         eqpt = Podio.retorna_dicionario_equipamento_colaborador(func_obj.nome)
@@ -113,36 +89,10 @@ def iterar_dataframe():
         if dict_b['patrimonios'] != 'empty':
             new_dict = {**dict_a, **dict_b}
             print(new_dict)
+    #         df_dictionary = pd.DataFrame([new_dict])
+    #         output = pd.concat([output, df_dictionary], ignore_index=True)
+    # print(output)
 
-
-
-def monta_funcionario_demitido(row):
-    func_obj = Demitidos.verifica_demitido(row['Nome Funcionários'])
-    return print(func_obj)
-
-def email_superior_override(row):
-    chefe = row['Nome Superior']
-    email_chefe = dataframe_chefia.loc[chefe]
-    setter = Demitidos()
-    setter.email_chefia(email_chefe)
-    return
-
-
-
-# def compara_dataframe_demitidos_atuais(chefe):
-#     return dataframe_chefia.loc[chefe]
 
 if __name__ == '__main__':
     iterar_dataframe()
-
-
-#
-#
-#
-#
-#
-#
-# res = {key: dict_gigante[key] for key in dict_gigante.keys() & {'items'}}
-# new_res = res['items'][0]
-# new_dictão = {key: new_res[key] for key in new_res.keys() & {'fields'}}
-# new_new_dictão = new_dictão['fields']
