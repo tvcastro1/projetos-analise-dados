900f6d4a4dad1142522f90dfa2c7144d510e1848
tvcastro1 <thiagovieirac@gmail.com>
2022-11-04 17:38:18 -0300

    Função que chama a função de envio de email

--- citrix-podio/demitidos/main.py
+++ citrix-podio/demitidos/main.py
@@ -3,6 +3,7 @@ from demitidos import Demitido
 from podio import Tablet
 from db_local import get_entry
 from dataframes import DATAFRAME_DEMITIDOS
+from emailer import email_sender
 
 
 def to_csv():
@@ -21,5 +22,21 @@ def to_csv():
                 # print(f'Demitido {demitido.nome} em {demitido.dt_demissao}  com equipamento de patrimônio: '
                 #       f'{equipamento.patrimonio}, status: {equipamento.status}')
 
+
+def to_email():
+    x = 0
+    df = pd.DataFrame()
+    for _, row in DATAFRAME_DEMITIDOS.iterrows():
+        demitido = Demitido.verifica_demitido(row['Nome Funcionários'])
+        for entry in get_entry():
+            equipamento = Tablet(*entry)
+            if equipamento.utilizador == demitido.nome and equipamento.status == 'Com o colaborador' \
+                    and demitido.email_superior != None:
+                dicio = {'demitido': demitido.nome, 'dt_demissao': demitido.dt_demissao,
+                         'patrimonio': equipamento.patrimonio, 'status': equipamento.status, 'email_chefe': demitido.
+                        email_superior}
+                email_sender(dicio['email_chefe'], dicio['patrimonio'])
+
+
 if __name__ == '__main__':
-    to_csv()
+    to_email()
