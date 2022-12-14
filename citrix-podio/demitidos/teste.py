fb1b3008bf0f1c97a30cb22956232e8d8c5ea7df
tvcastro1 <thiagovieirac@gmail.com>
2022-12-14 02:59:01 -0300

    Refatoração. Definição de método de classe e métodos estáticos

--- citrix-podio/demitidos/teste.py
+++ citrix-podio/demitidos/teste.py
@@ -1,33 +1,17 @@
 import sqlite3
 import pandas as pd
 from queries import QUERY_SELECT_ALL_TABLETS
-#
-# conn = sqlite3.connect('podio.db')
-#
-# sql_query = pd.read_sql_query('''
-#                                SELECT
-#                                *
-#                                FROM tablets
-#                                ''', conn)
 
 
-# df_podio = pd.DataFrame(sql_query)
-# df_demitidos = pd.read_csv('demitidos.csv', dtype=str).drop_duplicates()
-#
-# df_merged = pd.merge(df_podio, df_demitidos, how='inner', left_on='nome-colaborador', right_on='Nome Funcionários')
-# df_merged_final = df_merged[df_merged.statusDesc == 'Com o colaborador']
-# print(df_merged_final)
+class DataframeDemitidos(object):
+    dataframe_demitidos_s_filtro = pd.DataFrame()
 
-class DataManipulation(object):
-    def __init__(self):
-        self.df_demitidos = self.merged_df_demitidos_atuais_podio()
-        self.df_podio = None
-
-    @staticmethod
-    def merged_df_demitidos_atuais_podio() -> pd.DataFrame:
-        df_demitidos = DataManipulation.__csv_demitidos_para_df()
-        df_atuais = DataManipulation.__csv_atuais_para_df()
-        df_podio_tablets = DataManipulation.__sql_query_podio_tablets_para_df()
+    @classmethod
+    def carrega_df_demitidos(cls):
+        df_demitidos = cls.__csv_demitidos_para_df()
+        df_atuais = cls.__csv_atuais_para_df()
+        df_podio_tablets = cls.__sql_query_podio_tablets_para_df()
+        # Bloco de Merge
         df_merged_demitidos = pd.merge(df_demitidos, df_atuais, how='left', left_on='Nome Superior',
                                        right_on='NOMEFUNCIONARIO', indicator=True)
         df_merged_demitidos_podio_tablets = pd.merge(df_merged_demitidos, df_podio_tablets, how='inner',
@@ -37,12 +21,14 @@ class DataManipulation(object):
                                                                                'Nome Superior', 'EMAIL',
                                                                                'patrimonio-novo', 'modeloDesc',
                                                                                'statusDesc']]
-        df_merged_demitidos_podio_tablets = df_merged_demitidos_podio_tablets.rename \
-            (columns={'Nome Funcionários': 'nome_funcionarios', 'Data Demissão': 'data_demissao',
-                      'Nome Superior': 'nome_superior',
-                      'EMAIL': 'email', 'patrimonio-novo': 'patrimonio_novo', 'modeloDesc': 'modelo_desc',
-                      'statusDesc': 'status_desc'})
-        return df_merged_demitidos_podio_tablets
+        # Bloco de tratamento de cabeçalho do DF
+        df_merged_demitidos_podio_tablets = df_merged_demitidos_podio_tablets.rename(
+            columns={'Nome Funcionários': 'nome_funcionarios', 'Data Demissão': 'data_demissao',
+                     'Nome Superior': 'nome_superior',
+                     'EMAIL': 'email', 'patrimonio-novo': 'patrimonio_novo', 'modeloDesc': 'modelo_desc',
+                     'statusDesc': 'status_desc'})
+        cls.dataframe_demitidos_s_filtro = df_merged_demitidos_podio_tablets.fillna('null')
+        return cls
 
     @staticmethod
     def __csv_demitidos_para_df() -> pd.DataFrame:
@@ -54,23 +40,51 @@ class DataManipulation(object):
 
     @staticmethod
     def __csv_atuais_para_df() -> pd.DataFrame:
-        """Inicializa dataframe atuais e retorna df"""
+        """Parsing de funcionários atuais para DF"""
         big_atuais = pd.read_csv('atuais.csv', dtype=str)
         big_atuais = big_atuais[['NOMEFUNCIONARIO', 'EMAIL']]
         return big_atuais
 
     @staticmethod
     def __sql_query_podio_tablets_para_df() -> pd.DataFrame:
+        """Parsing sql query Podio para DF"""
         conn = sqlite3.connect('podio.db')
         df_sql_query = pd.read_sql_query(QUERY_SELECT_ALL_TABLETS, conn)
         return df_sql_query
 
-    def save_to_csv(self):
-        self.df_demitidos.to_excel('dfteste.xlsx', encoding='utf-8')
+    @staticmethod
+    def salva_para_xlsx(dataframe=None) -> None:
+        dataframe.to_excel('demitidos_s_chefe.xlsx', encoding='utf-8')
+        return None
+
+    def __init__(self):
+        self.df_concat = None
+        self.df_demitidos_sem_chefia = None
+
+    def group_concat_df(self):
+        # self.df_concat = self.dataframe_demitidos_s_filtro.query("status_desc == 'Com o colaborador'")
+        self.df_concat = self.dataframe_demitidos_s_filtro.groupby(['nome_funcionarios'], as_index=False).agg(
+            {'patrimonio_novo': ', '.join})
+        self.salva_para_xlsx(self.df_concat)
+
+    def df_sem_chefia(self):
+        df = self.dataframe_demitidos_s_filtro.query("nome_superior != 'null'")
+        self.salva_para_xlsx(df)
+        self.df_demitidos_sem_chefia = df
+
+        print(df)
+
+    def __str__(self) -> str:
+        return self.df_demitidos_sem_chefia.to_string()
 
 
 if __name__ == '__main__':
-    obj = DataManipulation()
-    obj.save_to_csv()
-    print(obj.df_demitidos)
-    print('Fim da execução')
+    df = DataframeDemitidos.carrega_df_demitidos()
+    obj = DataframeDemitidos()
+    obj.group_concat_df()
+    obj.df_sem_chefia()
+
+    # obj = DataframeDemitidos()
+    # obj.save_to_csv()
+    # print(obj.df_demitidos)
+    # print('Fim da execução')
